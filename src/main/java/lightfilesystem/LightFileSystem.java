35429e9098067d34cda8b6808571567a79202f76
BodaZsolt <zsolt.boda@straxus.hu>
2021-05-04 15:22:59 +0200

    [NEW] Added plugins: Lombok, TinyLog, Jackson, JavaDoc, Exec and their dependencies and configurations

---
+++ src/main/java/lightfilesystem/LightFileSystem.java
@@ -0,0 +1,9 @@
+package lightfilesystem;
+import org.tinylog.Logger;
+
+public class LightFileSystem {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!");
+        Logger.info("We are running boys!!!!");
+    }
+}
