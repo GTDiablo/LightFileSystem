b33d5a2b6eab1a2feb0f24906c507cfe8d1292c6
BodaZsolt <zsolt.boda@straxus.hu>
2021-05-09 15:37:16 +0200

    [NEW] Added JUNIT 5 and test package

---
+++ src/test/java/lightfilesystem/AccessCheckerTest.java
@@ -0,0 +1,13 @@
+package lightfilesystem;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AccessCheckerTest {
+
+    @Test
+    public void testUserAccess(){
+        Assertions.assertEquals(1, 1);
+        // Assertions.assertFalse(true);
+    }
+}
